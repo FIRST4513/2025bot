ec54fe55fd2db6885c30e31c5953012ffb7c13f7
Jon Glick <593022+glickbot@users.noreply.github.com>
2025-04-22 16:06:07 -0700

    added file

---
+++ src/main/java/frc/robot/auto/AutoTelemetry.java
@@ -0,0 +1,76 @@
+package frc.robot.auto;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
+import edu.wpi.first.networktables.GenericEntry;
+import java.util.Map;
+
+public class AutoTelemetry {
+    private ShuffleboardTab tab;
+    private SimpleWidget translationPWidget;
+    private SimpleWidget translationIWidget;
+    private SimpleWidget translationDWidget;
+    private SimpleWidget rotationPWidget;
+    private SimpleWidget rotationIWidget;
+    private SimpleWidget rotationDWidget;
+
+    public AutoTelemetry() {
+        tab = Shuffleboard.getTab("Auto PID");
+        
+        // Create widgets for translation PID constants
+        translationPWidget = tab.add("Translation P", 0.1)
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0.0, "max", 5.0, "block increment", 0.1))
+            .withPosition(0, 0);
+            
+        translationIWidget = tab.add("Translation I", 0.0)
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0.0, "max", 5.0, "block increment", 0.1))
+            .withPosition(0, 1);
+            
+        translationDWidget = tab.add("Translation D", 0.0)
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0.0, "max", 5.0, "block increment", 0.1))
+            .withPosition(0, 2);
+
+        // Create widgets for rotation PID constants
+        rotationPWidget = tab.add("Rotation P", 1.5)
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0.0, "max", 5.0, "block increment", 0.1))
+            .withPosition(1, 0);
+            
+        rotationIWidget = tab.add("Rotation I", 0.5)
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0.0, "max", 5.0, "block increment", 0.1))
+            .withPosition(1, 1);
+            
+        rotationDWidget = tab.add("Rotation D", 0.5)
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0.0, "max", 5.0, "block increment", 0.1))
+            .withPosition(1, 2);
+    }
+
+    public void update() {
+        // Get the current values from NetworkTables
+        double translationP = translationPWidget.getEntry().getDouble(0.1);
+        double translationI = translationIWidget.getEntry().getDouble(0.0);
+        double translationD = translationDWidget.getEntry().getDouble(0.0);
+        double rotationP = rotationPWidget.getEntry().getDouble(1.5);
+        double rotationI = rotationIWidget.getEntry().getDouble(0.5);
+        double rotationD = rotationDWidget.getEntry().getDouble(0.5);
+
+        // Update the PID constants in Auto.java if they've changed
+        if (translationP != Auto.translationPID.getP() ||
+            translationI != Auto.translationPID.getI() ||
+            translationD != Auto.translationPID.getD() ||
+            rotationP != Auto.rotationPID.getP() ||
+            rotationI != Auto.rotationPID.getI() ||
+            rotationD != Auto.rotationPID.getD()) {
+            
+            Auto.translationPID.setPID(translationP, translationI, translationD);
+            Auto.rotationPID.setPID(rotationP, rotationI, rotationD);
+        }
+    }
+} 
